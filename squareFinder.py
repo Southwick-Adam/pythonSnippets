4d4edeff78fc0c3246b0eaf5984f282974fec371
Adam Southwick <adamsouthwick@pc-134-91.customer.ask4.lan>
2023-10-03 20:15:21 +0100

    scaled sq finder

--- squareFinder.py
+++ squareFinder.py
@@ -1,26 +1,33 @@
 num = float(input("please enter a number: "))
 exact = int(input("how many decimals of precision do you want?: "))
 
-def shorten(num):
-    return round(num,exact)
-
 if num < 0:
     print("Error: must be positive.")
     exit()
-i = 0
+
+calcs = 0
+stepSize = 0
 step = 1
-for k in range(0,exact + 1):
+
+#we can make it scale much more efficiently by testing the magnitude of the first step
+#before running the for loop
+while step < num:
+    stepSize += 1
+    calcs += 1
+    step = 10 ** stepSize
+  
+i = 0
+for k in range(0,exact + stepSize + 1):
     while i ** 2 < num:
         i += step
+        calcs += 1
     if i ** 2 == num:
         print(num,"is exactly",i,"squared")
         exit()
-    if k == exact:
-        print("the square root of",num,"is between",shorten(i-step),"and",shorten(i))
+    if k == exact + stepSize:
+        print("the square root of",num,"is approx", round(i,exact))
         exit()
     #if were looping, take i back a step and reduce the step size 
     i -= step
     step /= 10
-
-
     
